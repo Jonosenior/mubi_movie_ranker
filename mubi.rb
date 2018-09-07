d464b36f3a4e04d72ec852d4fd6d5cdc68cda6d1
jonathan senior <jonosenior@gmail.com>
2018-09-07 11:04:26 +0200

    add working imdb api call

--- mubi.rb
+++ mubi.rb
@@ -2,12 +2,31 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
+require 'unirest'
+
 
 page_url = 'https://mubi.com/showing'
 page = Nokogiri::HTML(open(page_url))
 
-current_films = page.css('div#page-region a').select do |a|
+current_films_buttons = page.css('div#page-region a').select do |a|
   a['href'] if a['href'].match('/showing/')
 end
 
-puts current_films.count
+titles = current_films_buttons.map do |a|
+  a['href'].sub('/showing/', '')
+end
+
+
+
+# puts titles.count
+
+
+# IMDB api call
+
+imdb_key = '47d7375b'
+
+url = "http://www.omdbapi.com/?apikey=#{imdb_key}&t=the-wave"
+response = Unirest.get url
+puts response.body["Title"]
+puts response.body["imdbRating"]
+# puts response.body["Ratings"]["Source"]
