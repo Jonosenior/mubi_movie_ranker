e15c0331a5f3c1a6e8a1dc0b0eb12d21cc2f1760
jonathan senior <jonosenior@gmail.com>
2018-09-11 20:42:55 +0200

    Start troubleshooting 'can't match nil' issue

--- mubi.rb
+++ mubi.rb
@@ -22,16 +22,16 @@ def current_mubi_films
 
   film_title_elements = page.css('#page-region h2')
   film_year_elements = page.css('#page-region h3')
-  film_title_buttons = page.css('#page-region a').select { |a| a['href'] if a['href'].match(/showing/)}
+  # film_title_buttons = page.css('#page-region a').select { |a| a['href'] if a['href'].match(/showing/)}
 
   titles = film_title_elements.map { |a| a.text }
   years = film_year_elements.map { |b| b.text.match(/\d{4}/) }
-  ascii_dashed_titles = film_title_buttons.map { |a|  a['href'].sub('/showing/', '') }
+  # ascii_dashed_titles = film_title_buttons.map { |a|  a['href'].sub('/showing/', '') }
 
   films = (0..29).to_a.map do |i|
     { title: titles[i],
-      year: years[i].to_s,
-      ascii_dashed_title: ascii_dashed_titles[i]
+      year: years[i].to_s # ,
+      # ascii_dashed_title: ascii_dashed_titles[i]
     }
   end
 end
