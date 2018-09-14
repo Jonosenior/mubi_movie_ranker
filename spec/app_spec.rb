c7f79c5a9a5112f86a8c2d3082ca65636650c18a
jonathan senior <jonosenior@gmail.com>
2018-09-14 14:31:26 +0200

    Add rspec html matchers to gemfile and add two working tests

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe MubiMovieRanker do
-  it 'works' do
-    # ...
+  let(:app) { MubiMovieRanker.new }
+
+  context 'GET to /' do
+    let(:response) { get '/' }
+
+    it 'returns status 200 OK' do
+      expect(response.status).to eq 200
+    end
+
+    it 'returns 30 Mubi film titles' do
+      expect(response.body).to have_tag('a.film-title', count: 30)
+    end
   end
 end
