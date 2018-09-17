ebeface3d3aca8b3195e08c8ec56871f7eeaec90
jonathan senior <jonosenior@gmail.com>
2018-09-17 11:10:09 +0200

    Add unfinished tests for main app

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -14,8 +14,17 @@ describe MubiMovieRanker do
       expect(response.body).to have_tag('a.film-title', count: 30)
     end
 
-    it 'links to Mubi in each film title' do
-      expect(response.body.to have_link(href: '/mubi/')
+    it 'displays a rating for each film' do
+      expect(response.body).to have_tag('p.rating', count: 30)
     end
+
+    it 'displays the plot for each film'
+    it 'displays each film rating as decimal or "n/a"'
+    it 'links to Mubi in each film title'
+    it 'does not contain a broken link'
+    it 'links to the about page'
+    it 'has same title font for Navbar and Jumbotron'
+    it 'displays highest-rated film at the top'
+    it 'displays films with no rating at the bottom'
   end
 end
