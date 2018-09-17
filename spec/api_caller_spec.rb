5a2bcfb23d529ebb0707a707f4268f10044a8fa8
jonathan senior <jonosenior@gmail.com>
2018-09-17 11:09:45 +0200

    Add spec for new api caller class

---
+++ spec/api_caller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe APICaller do
+  subject(:retriever) { FilmRetriever }
+  let(:films) { retriever.current_mubi_films }
+
+  context 'Given a set of films' do
+    it ''
+  end
+end
